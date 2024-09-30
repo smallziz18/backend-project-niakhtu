75c87780b7f9521f632d94755416547f1bae008f
smallziz18 <abdoulaziz17.diouf@ucad.edu.sn>
2024-09-30 09:31:13 +0000

    gestion des plaignats finis avec la possibilité d'uploader un avatar

---
+++ src/test/java/com/niakhtu/backend/niakhtu/NiakhtuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niakhtu.backend.niakhtu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiakhtuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
